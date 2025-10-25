7b48f1908260c094dff69f308fc7d376eaf6a6a9
luisdogo <luisdogoms@gmail.com>
2025-10-25 16:49:00 -0600

    redundancy removed

---
+++ models/arima.py
@@ -0,0 +1,16 @@
+
+class ARIMA():
+    
+    def __init__(self, window):
+        # choose model
+        self.window = window
+        return 0
+    
+    def train(time_series : list) -> list:
+        # calculate model parameters
+        self.model = 0
+        return 0
+
+    def predict():
+        # model.predict
+        return [y_hat, confidence_interval]
